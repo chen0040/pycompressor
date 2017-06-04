0daf659ecfcd050a403b0419f0b3366d601d2039
xschen <xs0040@gmail.com>
2017-06-04 13:29:59 +0800

    minor bug fix

--- tests/huffman_unit_test.py
+++ tests/huffman_unit_test.py
@@ -23,7 +23,7 @@ class HuffmanCompressionUnitTest(unittest.TestCase):
                    'but also the leap into electronic typesetting, remaining essentially unchanged. It was ' \
                    'popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, ' \
                    'and more recently with desktop publishing software like Aldus PageMaker including versions of ' \
-                   'Lorem Ipsum. '
+                   'Lorem Ipsum.'
         print('before compression: ' + original)
         print('length: ' + str(len(original)))
         compressed = huffman.compress_to_string(original)
